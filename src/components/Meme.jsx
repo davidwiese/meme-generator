18522ce0f79f699c4677a3e7af78aa681ad6a38c
David Wiese <21321101+davidwiese@users.noreply.github.com>
2023-12-22 13:05:50 -0800

    Adjust copy

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 function Meme() {
 	const [meme, setMeme] = useState({
 		topText: "ac",
-		bottomText: "b",
+		bottomText: "bd",
 		randomImage: "http://i.imgflip.com/1bij.jpg",
 	});
 	const [allMemes, setAllMemes] = useState([]);
