4f376caed3303b8c21f9030765e2cd5100dea4b6
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-01-05 13:10:04 -0800

    Remove placeholder again

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 function Meme() {
 	const [meme, setMeme] = useState({
 		topText: "",
-		bottomText: "a",
+		bottomText: "",
 		randomImage: "http://i.imgflip.com/1bij.jpg",
 	});
 	const [allMemes, setAllMemes] = useState([]);
